3abaa0a88bd85127dab410b02a1a9147da04a3cc
shahruhjumamyradov <shahruhjumamyradov@gmail.com>
2024-11-21 19:49:05 +0300

    Add files via upload

---
+++ Лабараторная 3/Задания 3.py
@@ -0,0 +1,14 @@
+a=input("Введите целое число \n")
+b=input("Введите целое число \n")
+c=input("Введите целое число \n")
+if a<b:
+    if a<c:
+        y=a
+else:
+     y=c
+
+if b<c:
+ y=b
+else:
+         y=c
+print("Минимальное:",y)
